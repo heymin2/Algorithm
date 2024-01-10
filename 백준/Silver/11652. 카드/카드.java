c1aa54c8040bb0322bc8284a5d4d292489347690
혜민 <97522726+heymin2@users.noreply.github.com>
2024-01-11 00:10:41 +0900

    [Silver IV] Title: 카드, Time: 388 ms, Memory: 36392 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,17 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         Map<Long, Integer> map = new HashMap<>();
 
         for(int i = 0; i < N; i++){
-            Long num = sc.nextLong();
+            Long num = Long.parseLong(br.readLine());
 
             map.put(num, map.getOrDefault(num, 0)+1);
         }
