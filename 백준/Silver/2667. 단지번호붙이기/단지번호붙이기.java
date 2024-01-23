27dbc343047bbe6bf12c655072313106c65f921e
혜민 <97522726+heymin2@users.noreply.github.com>
2024-01-23 16:40:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static int N;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visit = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 1 && !visit[i][j]) {
+                    check = 0;
+                    dfs(i, j);
+                    list.add(check);
+                }
+            }
+        }
+
+        list.sort(null);
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    static void dfs(int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= N || arr[x][y] == 0 || visit[x][y]) {
+            return;
+        }
+
+        visit[x][y] = true;
+        check++;
+
+        dfs(x + 1, y);
+        dfs(x - 1, y);
+        dfs(x, y + 1);
+        dfs(x, y - 1);
+    }
+}
