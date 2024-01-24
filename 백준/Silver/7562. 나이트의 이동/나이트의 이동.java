a5fdcd717c160d183372cfb8ae1f408a88fa0322
혜민 <97522726+heymin2@users.noreply.github.com>
2024-01-24 15:47:17 +0900

    [Silver I] Title: 나이트의 이동, Time: 456 ms, Memory: 73452 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -8,52 +8,58 @@ import java.util.StringTokenizer;
 public class Main {
     static int[][] arr;
     static boolean[][] visit;
+    static int[] x = { 1, 1, -1, -1, 2, 2, -2, -2 };
+    static int[] y = { 2, -2, 2, -2, 1, -1, 1, -1 };
+
+    static int I, startX, startY, endX, endY;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[] x = { 1, 1, -1, -1, 2, 2, -2, -2 };
-        int[] y = { 2, -2, 2, -2, 1, -1, 1, -1 };
 
         for (int t = 0; t < T; t++) {
-            int I = Integer.parseInt(br.readLine());
+            I = Integer.parseInt(br.readLine());
             arr = new int[I][I];
             visit = new boolean[I][I];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
+            startX = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
 
-            int endX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
+            endX = Integer.parseInt(st.nextToken());
+            endY = Integer.parseInt(st.nextToken());
 
-            Queue<Chess> q = new LinkedList<>();
+            System.out.println(bfs());
+        }
+    }
 
-            q.offer(new Chess(startX, startY, 0));
+    static int bfs() {
+        Queue<Chess> q = new LinkedList<>();
 
-            while (!q.isEmpty()) {
-                Chess c = q.poll();
+        q.offer(new Chess(startX, startY, 0));
 
-                if (c.x == endX && c.y == endY) {
-                    System.out.println(c.cnt);
-                    break;
-                }
+        while (!q.isEmpty()) {
+            Chess c = q.poll();
 
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 8; j++) {
-                        int nextX = c.x + x[i];
-                        int nextY = c.y + y[i];
-                        if (nextX >= 0 && nextY >= 0 && nextX < I && nextY < I && !visit[nextX][nextY]) {
-                            visit[nextX][nextY] = true;
-                            q.offer(new Chess(nextX, nextY, c.cnt + 1));
-                        }
+            if (c.x == endX && c.y == endY) {
+                return c.cnt;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    int nextX = c.x + x[i];
+                    int nextY = c.y + y[i];
+                    if (nextX >= 0 && nextY >= 0 && nextX < I && nextY < I && !visit[nextX][nextY]) {
+                        visit[nextX][nextY] = true;
+                        q.offer(new Chess(nextX, nextY, c.cnt + 1));
                     }
                 }
             }
         }
+        return 0;
     }
 }
 
