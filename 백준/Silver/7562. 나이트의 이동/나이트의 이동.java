4157860112e9f427105f850d846d00824d98487f
혜민 <97522726+heymin2@users.noreply.github.com>
2024-01-24 15:38:08 +0900

    [Silver I] Title: 나이트의 이동, Time: 752 ms, Memory: 76816 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+    static boolean[][] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] x = { 1, 1, -1, -1, 2, 2, -2, -2 };
+        int[] y = { 2, -2, 2, -2, 1, -1, 1, -1 };
+
+        for (int t = 0; t < T; t++) {
+            int I = Integer.parseInt(br.readLine());
+            arr = new int[I][I];
+            visit = new boolean[I][I];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
+
+            Queue<Chess> q = new LinkedList<>();
+
+            q.offer(new Chess(startX, startY, 0));
+
+            while (!q.isEmpty()) {
+                Chess c = q.poll();
+
+                if (c.x == endX && c.y == endY) {
+                    System.out.println(c.cnt);
+                    break;
+                }
+
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        int nextX = c.x + x[i];
+                        int nextY = c.y + y[i];
+                        if (nextX >= 0 && nextY >= 0 && nextX < I && nextY < I && !visit[nextX][nextY]) {
+                            visit[nextX][nextY] = true;
+                            q.offer(new Chess(nextX, nextY, c.cnt + 1));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Chess {
+    int x;
+    int y;
+    int cnt;
+
+    public Chess(int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+    }
+}
