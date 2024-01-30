7e5d2f382c466dad05cb0915fb03991732c8e7ec
혜민 <97522726+heymin2@users.noreply.github.com>
2024-01-30 11:36:26 +0900

    [Silver III] Title: N과 M (1), Time: 336 ms, Memory: 34624 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] check;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        check = new boolean[N + 1];
+
+        re(0);
+        System.out.println(sb);
+    }
+
+    static void re(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!check[i]) {
+                arr[cnt] = i;
+                check[i] = true;
+                re(cnt + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
