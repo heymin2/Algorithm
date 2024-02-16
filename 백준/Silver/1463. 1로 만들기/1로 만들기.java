c8be5c4868441a1bf328b5aecc6e80631746b30e
혜민 <97522726+heymin2@users.noreply.github.com>
2024-02-16 15:13:28 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[N+1];
+        PriorityQueue<num> pq = new PriorityQueue<>();
 
-        for(int i = 2; i <= N; i++){
-            dp[i] = dp[i-1] + 1;
-            if(i%2 == 0){
-                dp[i] = Math.min(dp[i], dp[i/2] + 1);
+        pq.offer(new num(N, 0));
+
+        int[] visit = new int[N + 1];
+
+        int[] dx = { 2, 3, -1 };
+        int next;
+
+        while (!pq.isEmpty()) {
+            num now = pq.poll();
+
+            sb.append(now.x);
+
+            if (now.x == 1) {
+                System.out.println(now.cnt);
+                break;
             }
-            if(i%3 == 0){
-                dp[i] = Math.min(dp[i],dp[i/3] + 1);
+
+            for (int i = 0; i < 3; i++) {
+                if (i == 2) {
+                    next = now.x + dx[i];
+                    if (visit[next] == 0) {
+                        visit[next] = now.cnt + 1;
+                        pq.offer(new num(next, now.cnt + 1));
+                    } else if (visit[next] > now.cnt + 1) {
+                        visit[next] = now.cnt + 1;
+                        pq.offer(new num(next, now.cnt + 1));
+                    }
+                } else {
+                    next = now.x / dx[i];
+                    if (now.x % dx[i] == 0) {
+                        if (visit[next] == 0) {
+                            visit[next] = now.cnt + 1;
+                            pq.offer(new num(next, now.cnt + 1));
+                        } else if (visit[next] > now.cnt + 1) {
+                            visit[next] = now.cnt + 1;
+                            pq.offer(new num(next, now.cnt + 1));
+                        }
+                    }
+                }
             }
         }
-        System.out.println(dp[N]);
+    }
+}
+
+class num implements Comparable<num> {
+    int x, cnt;
+
+    num(int x, int cnt) {
+        this.x = x;
+        this.cnt = cnt;
+    }
+
+    @Override
+    public int compareTo(num o) {
+        return this.cnt - o.cnt;
     }
 }
