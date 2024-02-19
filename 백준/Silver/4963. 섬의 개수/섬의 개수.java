9bf49298da1013296f8a957d513a0ce5aa1c8c17
혜민 <97522726+heymin2@users.noreply.github.com>
2024-02-19 17:12:25 +0900

    [Silver II] Title: 섬의 개수, Time: 164 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,62 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int w,h;
-    static int[][] arr;
-    static boolean[][] visit;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	static int count, w, h;
+	static int[][] map;
+	static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1},
+			{-1, 1}, {1, 1}, {1, -1}, {-1, -1}}; //상하좌우,우상,우하,좌하,좌상
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			count = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken()); //열개수
+			h = Integer.parseInt(st.nextToken()); //행개수
 
-        while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
+			if (h == 0)
+				break;
+			
+			map = new int[h][w];
 
-            if(w == 0 && h == 0){
-                break;
-            }
+			for (int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            arr = new int[h][w];
-            visit = new boolean[h][w];
-
-            for(int i = 0; i < h; i++){
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < w; j++){
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            int cnt = 0;
-
-            for(int i = 0; i < h; i++){
-                for(int j = 0; j < w; j++){
-                    if(arr[i][j] == 1 && !visit[i][j]){
-                        cnt++;
-                        dfs(i, j);
-                    }
-                }
-            }
-            sb.append(cnt).append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    static void dfs(int x, int y){
-        if(x < 0 || y < 0 || x >= h || y >= w || visit[x][y] || arr[x][y] == 0) return;
-
-        visit[x][y] = true;
-
-        dfs(x+1, y);
-        dfs(x-1, y);
-        dfs(x, y+1);
-        dfs(x, y-1);
-        dfs(x+1, y+1);
-        dfs(x+1, y-1);
-        dfs(x-1, y-1);
-        dfs(x-1, y+1);
-    }
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if (map[i][j] == 1) {
+						count++;
+						dfs(i, j);
+					}
+				}
+			}
+			sb.append(count).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static void dfs(int i, int j) {
+		map[i][j] = 2;
+		
+		for (int d = 0; d < delta.length; d++) {
+			int ni = i + delta[d][0];
+			int nj = j + delta[d][1];
+			
+			if (ni >= 0 && ni < h && nj >= 0 && nj < w && map[ni][nj] == 1) {
+				dfs(ni, nj);
+			}
+		}
+	}
 }
