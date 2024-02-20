974f46a256587f91799d6ef820089b325be6782f
혜민 <97522726+heymin2@users.noreply.github.com>
2024-02-20 23:51:30 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 432 ms, Memory: 50400 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); // 도시의 개수
+        int M = Integer.parseInt(br.readLine()); // 버스의 개수
+
+        List<bus>[] graph = new ArrayList[N+1];
+        int[] visit = new int[N+1];
+        int[] prev = new int[N+1];
+
+        for(int i = 1; i <= N; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int pay = Integer.parseInt(st.nextToken());
+
+            graph[s].add(new bus(e, pay));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        Arrays.fill(visit, Integer.MAX_VALUE);
+
+        PriorityQueue<bus> pq = new PriorityQueue<>();
+        visit[start] = 0;
+        pq.offer(new bus(start, 0));
+
+        while (!pq.isEmpty()) {
+            bus now = pq.poll();
+
+            if(now.x == end){
+                break;
+            }
+
+            for(bus next : graph[now.x]){
+                if(visit[next.x] > now.pay + next.pay){
+                    visit[next.x] = now.pay + next.pay;
+
+                    prev[next.x] = now.x;
+                    pq.offer(new bus(next.x, visit[next.x]));
+                }
+            }
+        }
+
+        sb.append(visit[end]).append("\n");
+
+        // 경로 구하기
+        Stack<Integer> s = new Stack<>();
+        s.push(end);
+
+        while(prev[end] != 0){
+            s.push(prev[end]);
+            end = prev[end];
+        }
+
+        sb.append(s.size()).append("\n");
+
+        while(!s.isEmpty()){
+            sb.append(s.pop()).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
+
+class bus implements Comparable<bus> {
+    int x, pay;
+
+    bus(int x, int pay) {
+        this.x = x;
+        this.pay = pay;
+    }
+
+    @Override
+    public int compareTo(bus o) {
+        return this.pay - o.pay;
+    }
+}
