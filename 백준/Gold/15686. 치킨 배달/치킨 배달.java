a517966fee5f2fec66c6337e83cebfc561bafd69
혜민 <97522726+heymin2@users.noreply.github.com>
2024-02-21 00:04:13 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 15344 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,121 +2,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static List<int[]> twoIdxList = new ArrayList<>();
-	static List<int[]> oneIdxList = new ArrayList<>();
-	static List<int[]> comb = new ArrayList<>();
-	static int N, M, sum, min;
-	static int[] arr;
-	static int[] di = { -1, 1, 0, 0 };
-	static int[] dj = { 0, 0, -1, 1 };
-	static int[][] map;
-	static boolean[] selected;
+    static int N, M, arr[][], now_chicken[], result = Integer.MAX_VALUE;
+    static List<int[]> chicken = new ArrayList<>();
+    static List<int[]> home = new ArrayList<>();
+    static int[] dx = { 1, -1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken()); // 개수
-		arr = new int[M];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		map = new int[N][N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int k = Integer.parseInt(st.nextToken());
-				if (k == 2) {
-					twoIdxList.add(new int[] { i, j });
-					map[i][j] = 0;
-				} else if (k == 1) {
-					oneIdxList.add(new int[] { i, j, 0 });
-					map[i][j] = 1;
-				} else {
-					map[i][j] = 0;
-				}
-			}
-		}
-//		for (int[] a : oneIdxList) {
-//			System.out.println(Arrays.toString(a));
-//		}
-		selected = new boolean[twoIdxList.size()];
-		min = Integer.MAX_VALUE;
-		combi(0, 0);// comb에 2인 것 모음 idx 리스트들 저장
-		/*
-		 * 출력확인 System.out.println("==="); for(int[] co : comb) {
-		 * System.out.println(Arrays.toString(co)); } System.out.println("===");
-		 * System.out.println(twoIdxList.size());
-		 */
-		/*
-		for (int[] co : comb) {
-			sum = 0;
+        arr = new int[N][N];
+        now_chicken = new int[M]; // 폐업 후 치킨집
 
-			for (int c : co) {// 경우의 수 하나
-				map[twoIdxList.get(c)[0]][twoIdxList.get(c)[1]] = 2;
-			}
-//			for (int[] row : map) {
-//				System.out.println(Arrays.toString(row));
-//			}
-			//1인 것마다 bfs
-			for (int[] a : oneIdxList) {
-				bfs(a);
-			}
-			//2인 것 원상복구
-			for (int c : co) {
-				map[twoIdxList.get(c)[0]][twoIdxList.get(c)[1]] = 0;
-			}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
 
-			if (sum < min)
-				min = sum;
-		}
-		*/
+                if (arr[i][j] == 2) {
+                    chicken.add(new int[] { i, j });
+                }
 
-		System.out.println(min);
+                else if (arr[i][j] == 1) {
+                    home.add(new int[] { i, j });
+                }
+            }
+        }
+        combination(0, 0);
 
-	}
+        System.out.println(result);
+    }
 
-	static void combi(int start, int depth) {
-		if (depth == M) {
-			int check = 0;
-			for (int[] a : oneIdxList) check += bfs(a);
-			if (check < min) min = check;
-			return;
-		}
+    static void combination(int cnt, int start) {
+        if (cnt == M) {
+            result = Math.min(result, checkD());
+            return;
+        }
 
-		for (int i = start; i < twoIdxList.size(); i++) {
-			map[twoIdxList.get(i)[0]][twoIdxList.get(i)[1]] = 2;
-			arr[depth] = i;
-			combi(i + 1, depth + 1);
-			map[twoIdxList.get(i)[0]][twoIdxList.get(i)[1]] = 0;
-		}
-	}
+        for (int i = start; i < chicken.size(); i++) {
+            now_chicken[cnt] = i;
+            combination(cnt + 1, i + 1);
+        }
+    }
 
-	static int bfs(int[] a) {// bfs
-		Queue<int[]> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N][N];
-		queue.offer(a);
-		
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int dist = cur[2];
+    static int checkD() {
+        int total = 0;
+        for (int i = 0; i < home.size(); i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < M; j++) {
+                int[] h = home.get(i);
+                int[] c = chicken.get(now_chicken[j]);
+                int d = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
+                if (d < min) {
+                    min = d;
+                }
+            }
+            total += min;
 
-			for (int d = 0; d < 4; d++) {
-				int ni = cur[0] + di[d];
-				int nj = cur[1] + dj[d];
+        }
 
-				if (ni >= 0 && ni < N && nj >= 0 && nj < N && !visited[ni][nj]) {
-					if (map[ni][nj] == 2) return dist + 1;
-					visited[ni][nj] = true;
-					queue.offer(new int[] { ni, nj, dist + 1 });
-				}
-			}
-		}
-		return 0;
-	}
+        return total;
+    }
 }
