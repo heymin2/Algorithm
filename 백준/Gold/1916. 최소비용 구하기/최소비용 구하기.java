54f7ba7e8009f18d45e0d49cc0a4e4ec14e6a641
혜민 <97522726+heymin2@users.noreply.github.com>
2024-02-20 23:53:31 +0900

    [Gold V] Title: 최소비용 구하기, Time: 436 ms, Memory: 50568 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,42 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine()); // 도시의 개수
         int M = Integer.parseInt(br.readLine()); // 버스의 개수
 
-        int[][] arr = new int[N][N];
-        int[] visit = new int[N];
+        List<bus>[] graph = new ArrayList[N+1];
+        int[] visit = new int[N+1];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = -1;
-            }
-            visit[i] = Integer.MAX_VALUE;
+        for(int i = 1; i <= N; i++){
+            graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int s = Integer.parseInt(st.nextToken()) - 1;
-            int e = Integer.parseInt(st.nextToken()) - 1;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
             int pay = Integer.parseInt(st.nextToken());
 
-            if (arr[s][e] == -1 || arr[s][e] > pay) {
-                arr[s][e] = pay;
-            }
+            graph[s].add(new bus(e, pay));
         }
 
         st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken()) - 1;
-        int end = Integer.parseInt(st.nextToken()) - 1;
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        Arrays.fill(visit, Integer.MAX_VALUE);
 
         PriorityQueue<bus> pq = new PriorityQueue<>();
         visit[start] = 0;
@@ -45,15 +47,16 @@ public class Main {
         while (!pq.isEmpty()) {
             bus now = pq.poll();
 
-            if (now.x == end) {
+            if(now.x == end){
                 System.out.println(now.pay);
                 break;
             }
 
-            for (int i = 0; i < N; i++) {
-                if (arr[now.x][i] != -1 && visit[i] > now.pay) {
-                    visit[i] = now.pay + arr[now.x][i];
-                    pq.offer(new bus(i, now.pay + arr[now.x][i]));
+            for(bus next : graph[now.x]){
+                if(visit[next.x] > now.pay + next.pay){
+                    visit[next.x] = now.pay + next.pay;
+
+                    pq.offer(new bus(next.x, visit[next.x]));
                 }
             }
         }
@@ -61,8 +64,7 @@ public class Main {
 }
 
 class bus implements Comparable<bus> {
-    int x;
-    int pay;
+    int x, pay;
 
     bus(int x, int pay) {
         this.x = x;
