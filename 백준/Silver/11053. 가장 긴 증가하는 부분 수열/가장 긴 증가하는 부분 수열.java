bdfae8ed3815979de520275c5fe06d2c2d6face3
혜민 <97522726+heymin2@users.noreply.github.com>
2024-03-27 12:18:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,38 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] arr, dp;
+    static int N, dp[];
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
         dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        dp[0] = Integer.parseInt(st.nextToken());
+        int max = 1; // 개수
 
-        dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i < N; i++){
-            dp[i] = 1;
-            for(int j = i-1; j >= 0; j--){
-                if(arr[j] < arr[i]){
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+            if (dp[max - 1] < num) {
+                dp[max] = num;
+                max++;
+            } else if (dp[max - 1] == num) {
+                continue;
+            } else {
+                int left = 0;
+                int right = max;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+
+                    if (dp[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
                 }
+
+                dp[right] = num;
             }
         }
 
-        Arrays.sort(dp);
-        System.out.println(dp[N-1]);
+        System.out.println(max);
     }
 }
