20389c729f2ab6701c717e45866104b733e8d0c8
혜민 <97522726+heymin2@users.noreply.github.com>
2024-04-05 16:46:29 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,102 +1,117 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] arr;
+    static int w, h, sx, sy, ex, ey;
+    static char map[][];
     static boolean[][] visit;
-    static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+    static int[] dx = { 1, -1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-        arr = new char[R][C];
-        visit = new boolean[R][C];
+        w = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
 
-        Queue<E> q = new LinkedList<>();
-        Queue<E> water = new LinkedList<>();
+        map = new char[w][h];
+        visit = new boolean[w][h];
+        Queue<int[]> list = new ArrayDeque<>();
 
-        for (int i = 0; i < R; i++) {
+        for (int i = 0; i < w; i++) {
             String str = br.readLine();
-            for (int j = 0; j < C; j++) {
-                arr[i][j] = str.charAt(j);
+            for (int j = 0; j < h; j++) {
+                map[i][j] = str.charAt(j);
 
-                if (arr[i][j] == 'S') {
-                    q.add(new E(i, j, 0));
-                    visit[i][j] = true;
+                if (map[i][j] == 'S') {
+                    sx = i;
+                    sy = j;
+                    map[i][j] = '.';
                 }
 
-                if (arr[i][j] == '*') {
-                    water.add(new E(i, j));
+                if (map[i][j] == 'D') {
+                    ex = i;
+                    ey = j;
+                }
+
+                if (map[i][j] == '*') {
+                    list.add(new int[] { i, j });
                 }
             }
         }
 
+        Queue<Node> q = new ArrayDeque<>();
+
+        q.offer(new Node(sx, sy, 0));
+        visit[sx][sy] = true;
+        int result = -1;
+
         while (!q.isEmpty()) {
-            int size = water.size();
-            for(int i = 0; i < size; i++){
-                E w = water.poll();
+            int size = list.size();
+            for (int i = 0; i < size; i++) {
+                int[] fire = list.poll();
 
                 for (int j = 0; j < 4; j++) {
-                    int nx = w.x + dx[j];
-                    int ny = w.y + dy[j];
-    
-                    if (nx < 0 || ny < 0 || nx >= R || ny >= C)
+                    int nx = fire[0] + dx[j];
+                    int ny = fire[1] + dy[j];
+
+                    if (nx < 0 || ny < 0 || nx >= w || ny >= h)
                         continue;
 
-                    if (arr[nx][ny] == '.'){
-                        water.offer(new E(nx, ny));
-                        arr[nx][ny] = '*';
-                    } 
+                    if (map[nx][ny] == '.') {
+                        map[nx][ny] = '*';
+                        list.offer(new int[] { nx, ny });
+                    }
                 }
             }
 
             size = q.size();
 
-            for(int i = 0; i < size; i++){
-                E now = q.poll();
+            for (int i = 0; i < size; i++) {
+                Node now = q.poll();
 
-                if (arr[now.x][now.y] == 'D') {
-                    
-                    System.out.println(now.cnt);
-                    System.exit(0);
+                if (now.x == ex && now.y == ey) {
+                    result = now.cnt;
+                    break;
                 }
 
                 for (int j = 0; j < 4; j++) {
                     int nx = now.x + dx[j];
                     int ny = now.y + dy[j];
 
-                    if (nx < 0 || ny < 0 || nx >= R || ny >= C)
+                    if (nx < 0 || ny < 0 || nx >= w || ny >= h)
                         continue;
-                    if (arr[nx][ny] == 'X' || visit[nx][ny] || arr[nx][ny] == '*')
+
+                    if (visit[nx][ny] || map[nx][ny] == '*' || map[nx][ny] == 'X')
                         continue;
-                    q.offer(new E(nx, ny, now.cnt + 1));
+
                     visit[nx][ny] = true;
+                    q.offer(new Node(nx, ny, now.cnt + 1));
                 }
             }
         }
-        System.out.println("KAKTUS");
+
+        if (result == -1) {
+            System.out.println("KAKTUS");
+        } else {
+            System.out.println(result);
+        }
     }
 }
 
-class E {
-    int x;
-    int y;
-    int cnt;
+class Node {
+    int x, y, cnt;
 
-    E(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    E(int x, int y, int cnt) {
+    Node(int x, int y, int cnt) {
         this.x = x;
         this.y = y;
         this.cnt = cnt;
