c7e0db396356d7f073756d036d11cfc75a309bea
혜민 <97522726+heymin2@users.noreply.github.com>
2024-05-06 14:34:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 247 ms, Memory: 60,888 KB -BaekjoonHub

---
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 요리사 - 4012 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIeUtVakTMDFAVH) 
+
+### 성능 요약
+
+메모리: 60,888 KB, 시간: 247 ms, 코드길이: 2,142 Bytes
+
+### 제출 일자
+
+2024-05-06 14:34
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Solution
+{
+	static int N, arr[][], min;
+    static int[] check;
+
+      public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int test = 1; test <= T; test++) {
+            N = Integer.parseInt(br.readLine());
+
+            arr = new int[N][N];
+            check = new int[N/2];
+
+            for(int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            min = Integer.MAX_VALUE;
+            select(0, 0);
+            sb.append("#").append(test).append(" ").append(min).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void select(int cnt, int start) {
+        if(cnt == N/2) {
+            List<Integer> A = new ArrayList<>();
+            List<Integer> B = new ArrayList<>();
+            
+            boolean[] visit  = new boolean[N];
+            for(int i = 0; i < N/2; i++) {
+                A.add(check[i]);
+                visit[check[i]] = true;
+            }
+
+            for(int i = 0; i < N; i++) {
+                if(!visit[i]) {
+                    B.add(i);
+                }
+            }
+
+            int s = Math.abs(sum(A) - sum(B));
+            min = Math.min(min, s);
+
+            return;
+        }
+
+        for(int i = start; i < N; i++) {
+            check[cnt] = i;
+            select(cnt+1, i+1);
+        }
+    }
+
+    static int sum(List<Integer> list) {
+        int s = 0;
+
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = 0; j < list.size(); j++) {
+                s += arr[list.get(i)][list.get(j)];
+            }
+        }
+
+        return s;
+    }
+}
