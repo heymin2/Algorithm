a2c9048fdbd665997c4ccd4181839e214b6e042f
혜민 <97522726+heymin2@users.noreply.github.com>
2024-05-07 09:46:42 +0900

    [Silver I] Title: 스타트와 링크, Time: 408 ms, Memory: 53180 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -4,69 +4,70 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-
-public class Main {
-    static int n, min= Integer.MAX_VALUE;
-    static int[][] arr;
-    static boolean[] selected;
-
-    public static void main(String[] args) throws IOException {
+ 
+class Main {
+    static int N, arr[][], min;
+    static int[] check;
+ 
+      public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-
-        arr = new int[n][n];
-        selected = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+        StringTokenizer st;
+ 
+        N = Integer.parseInt(br.readLine());
+ 
+        arr = new int[N][N];
+        check = new int[N/2];
+ 
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        select(0);
+ 
+        min = Integer.MAX_VALUE;
+        select(0, 0);
         System.out.println(min);
     }
-
-    static void select(int cnt) {
-        if (cnt == n) {
-            List<Integer> a = new ArrayList<>();
-            List<Integer> b = new ArrayList<>();
-
-            for (int i = 0; i < n; i++) {
-                if (selected[i]) {
-                    a.add(i);
-                } else {
-                    b.add(i);
+ 
+    static void select(int cnt, int start) {
+        if(cnt == N/2) {
+            List<Integer> A = new ArrayList<>();
+            List<Integer> B = new ArrayList<>();
+             
+            boolean[] visit  = new boolean[N];
+            for(int i = 0; i < N/2; i++) {
+                A.add(check[i]);
+                visit[check[i]] = true;
+            }
+ 
+            for(int i = 0; i < N; i++) {
+                if(!visit[i]) {
+                    B.add(i);
                 }
             }
-
-            if (a.size() == n/2 && b.size() <= n/2) {
-                int result = Math.abs(calculate(a) - calculate(b));
-                min = Math.min(min, result);
-                return;
-            }
-
+ 
+            int s = Math.abs(sum(A) - sum(B));
+            min = Math.min(min, s);
+ 
             return;
         }
-
-        selected[cnt] = true;
-        select(cnt + 1);
-        selected[cnt] = false;
-        select(cnt + 1);
+ 
+        for(int i = start; i < N; i++) {
+            check[cnt] = i;
+            select(cnt+1, i+1);
+        }
     }
-
-    static int calculate(List<Integer> list) {
-        int sum = 0;
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                int x = list.get(i);
-                int y = list.get(j);
-                sum += arr[x][y];
-                sum += arr[y][x];
+ 
+    static int sum(List<Integer> list) {
+        int s = 0;
+ 
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = 0; j < list.size(); j++) {
+                s += arr[list.get(i)][list.get(j)];
             }
         }
-        return sum;
+ 
+        return s;
     }
 }
