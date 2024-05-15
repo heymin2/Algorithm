aa22bb8dd3f9a3fb5f50e3a122152d05498e5ba3
혜민 <97522726+heymin2@users.noreply.github.com>
2024-05-15 21:46:47 +0900

    [Silver II] Title: N번째 큰 수, Time: 852 ms, Memory: 274104 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,23 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		Vector<Integer> v = new Vector<>();
-		
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
-				v.add(Integer.parseInt(st.nextToken()));
+				pq.add(Integer.parseInt(st.nextToken()));
 			}
 		}
-		
-		v.sort((o1, o2) -> o2 - o1);
-		
-		System.out.println(v.get(N-1));
-	}
+
+        while(N-- > 1){
+            pq.remove();
+        }
+
+        System.out.println(pq.peek());
+    }
 }
