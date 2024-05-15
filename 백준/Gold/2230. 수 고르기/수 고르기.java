60a5e29565f99b3591079fb602773cf96028f63f
혜민 <97522726+heymin2@users.noreply.github.com>
2024-05-15 21:57:10 +0900

    [Gold V] Title: 수 고르기, Time: 364 ms, Memory: 27856 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(A);
+
+        int left = 0;
+        int right = 1;
+
+        int result = Integer.MAX_VALUE;
+
+        while(true) {
+            if(right == N || left == N) {
+                break;
+            }
+
+            int value = A[right] - A[left];
+
+            if(value < M) {
+                right++;
+            }
+            else if(value == M) {
+                result = M;
+                break;
+            }
+            else {
+                result = Math.min(value, result);
+                left++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
