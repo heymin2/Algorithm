53e416b7d8b991112da16e2e2e8980186617b8ab
혜민 <97522726+heymin2@users.noreply.github.com>
2024-06-12 23:53:13 +0900

    [Gold V] Title: 꿀 따기, Time: 272 ms, Memory: 25160 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -1,22 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visit;
     static int N;
     static int[] arr, sum;
-    static int[] bee;
-    static int tong, max;
+    static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N+1];
         sum = new int[N+1];
-        bee = new int[2];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= N; i++) {
@@ -25,68 +21,29 @@ public class Main {
         }
 
         max = 0;
+        int total = 0;
 
-        for(int i = 1; i <= N; i++) {
-            visit = new boolean[N+1];
-            tong = i;
-            visit[i] = true;
-            select(0, 1);
+        // case1: 벌통 맨 오른쪽, 벌1 왼쪽 고정
+        for(int i = 2; i < N; i++) {
+            total = sum[N] - arr[1] - arr[i]; // 1번 벌
+            total += sum[N] - sum[i]; // 2번 벌
+            max = Math.max(total, max);
+        }
+      
+        // case2: 벌통 맨 왼쪽, 벌1 오른쪽 고정
+        for(int i = 2; i < N; i++) {
+            total = sum[N-1] - arr[i]; // 1번 벌
+            total += sum[N] - (sum[N] - sum[i-1]); // 2번 벌
+            max = Math.max(total, max);
+        }
+
+        // case3: 벌1 왼쪽, 벌2 오른쪽 고정
+        for(int i = 2; i < N; i++) {
+            total = sum[i] - arr[1];
+            total += sum[N] - sum[i-1] - arr[N];
+            max = Math.max(total, max);
         }
 
         System.out.println(max);
-    }
-
-    static void select(int idx, int start) {
-        if(idx == 2){
-            int total = 0;
-
-            int bee1 = bee[0];
-            int bee2 = bee[1];
-
-            // 첫번째 벌
-            if(bee1 > tong) {
-                total = sum[bee1] - sum[tong] - arr[bee1] + arr[tong];
-
-                if(bee2 > tong && bee2 < bee1) {
-                    total -= arr[bee2];
-                }
-            }
-            else{
-                total = sum[tong] - sum[bee1];
-
-                if(bee2 < tong && bee2 > bee1) {
-                    total -= arr[bee2];
-                }
-            }
-
-            // 두번째 벌
-            if(bee2 > tong) {
-                total += sum[bee2] - sum[tong] - arr[bee2] + arr[tong];
-
-                if(bee1 > tong && bee1 < bee2) {
-                    total -= arr[bee1];
-                }
-            }
-            else{
-                total += sum[tong] - sum[bee2];
-
-                if(bee1 < tong && bee1 > bee2) {
-                    total -= arr[bee1];
-                }
-            }
-
-            max = Math.max(total, max);
-            return;
-        }
-
-        for(int i = start; i <= N; i++) {
-            if(!visit[i]){
-                visit[i] = true;
-                bee[idx] = i;
-                select(idx+1, i);
-                visit[i] = false;
-            }
-        }
-
     }
 }
