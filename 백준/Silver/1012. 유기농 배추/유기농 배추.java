d0ae9d8c0e64146fbbbebaaec36b43209e1df4ba
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-01 16:17:58 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,58 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, K;
-    static boolean[][] arr;
-    static boolean[][] visit;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        while(T-- != 0) {
+            int cnt = 0;
 
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
 
-            arr = new boolean[N][M];
-            visit = new boolean[N][M];
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < K; i++) {
+            boolean[][] arr = new boolean[N][M];
+            boolean[][] check = new boolean[N][M];
+
+            for(int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int r = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
 
-                arr[r][c] = true;
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                arr[x][y] = true;
             }
 
-            int check = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (arr[i][j] && !visit[i][j]) {
-                        dfs(i, j);
-                        check++;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(arr[i][j] && !check[i][j]) {
+                        cnt++;
+
+                        Queue<int[]> q = new ArrayDeque<>();
+                        
+                        check[i][j] = true;
+                        q.add(new int[]{i,j});
+
+                        while(!q.isEmpty()) {
+                            int[] now = q.poll();
+
+                            for(int k = 0; k < 4; k++) {
+                                int nx = now[0] + dx[k];
+                                int ny = now[1] + dy[k];
+
+                                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                                if(!arr[nx][ny] || check[nx][ny]) continue;
+
+                                q.add(new int[]{nx, ny});
+                                check[nx][ny] = true;
+                            }
+                        }
                     }
                 }
             }
-            System.out.println(check);
+            System.out.println(cnt);
         }
-    }
-
-    static void dfs(int x, int y) {
-        if (x < 0 || y < 0 || x >= N || y >= M || !arr[x][y] || visit[x][y]) {
-            return;
-        }
-
-        visit[x][y] = true;
-
-        dfs(x + 1, y);
-        dfs(x - 1, y);
-        dfs(x, y + 1);
-        dfs(x, y - 1);
     }
 }
