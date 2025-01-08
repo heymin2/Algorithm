f5f0b4b9749c565dbbae75fd58f92c6f0e77eab0
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-08 18:55:41 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 116 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int min = 9999;
+    static int[][][] HP;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int check = 3 - list.size();
+
+        for(int i = 0; i < check; i++) {
+            list.add(0);
+        }
+
+        HP = new int[list.get(0)+1][list.get(1)+1][list.get(2)+1];
+
+        dfs(list.get(0), list.get(1), list.get(2), 0);
+
+        System.out.println(min);
+    }
+
+    static void dfs(int a, int b, int c, int num) {
+        if(a <= 0 && b <= 0 && c <= 0) {
+            min = Math.min(min, num);
+            return;
+        }
+
+        if(a < 0) a = 0;
+        if(b < 0) b = 0;
+        if(c < 0) c = 0;
+
+        if(HP[a][b][c] <= num && HP[a][b][c] != 0) return;
+
+        HP[a][b][c] = num;
+
+        dfs(a-9, b-3, c-1, num+1);
+        dfs(a-9, b-1, c-3, num+1);
+        dfs(a-3, b-9, c-1, num+1);
+        dfs(a-3, b-1, c-9, num+1);
+        dfs(a-1, b-9, c-3, num+1);
+        dfs(a-1, b-3, c-9, num+1);
+    }
+}
