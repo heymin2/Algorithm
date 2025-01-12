761729cc334d20e36a230723519efdfa920bdf55
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-12 12:04:42 +0900

    [Silver I] Title: 부등호, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -3,52 +3,57 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static boolean[] visit;
     static char[] arr;
-    static List<String> result = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
         arr = new char[N];
-        visit = new boolean[10];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             arr[i] = st.nextToken().charAt(0);
         }
 
-        dfs("", 0);
-
         StringBuilder sb = new StringBuilder();
-        sb.append(result.get(result.size()-1)).append("\n").append(result.get(0));
-        System.out.println(sb);
-    }
 
-    static void dfs(String num, int idx) {
-        if(idx == N + 1) {
-            result.add(num);
-            return;
-        }
+        ArrayDeque<Integer> q = new ArrayDeque<Integer>();
+        int num = 9;
+        q.push(num);
 
-        for(int i = 0; i <= 9; i++) {
-            if(visit[i]) continue;
-            if(idx == 0 || arrCheck(num.charAt(idx - 1) - '0', i, arr[idx -1])) {
-                visit[i] = true;
-                dfs(num + i, idx + 1);
-                visit[i] = false;
+        for(int i = 0; i < N; i++) {
+            num--;
+            if(arr[i] == '>') {
+                while(!q.isEmpty()) {
+                    sb.append(q.pop());
+                }
             }
+            q.push(num);
         }
-    }
 
-    static boolean arrCheck(int a, int b, char c) {
-        if(c == '<') {
-            if(a > b) return false;
-        } 
-        else if(c == '>') {
-            if(a < b) return false;
+        while(!q.isEmpty()) {
+            sb.append(q.poll());
         }
-        return true;
+
+        sb.append("\n");
+
+        num = 0;
+        q.push(num);
+
+        for(int i = 0; i < N; i++) {
+            num++;
+            if(arr[i] == '<') {
+                while(!q.isEmpty()) {
+                    sb.append(q.pop());
+                }
+            }
+            q.push(num);
+        }
+
+        while(!q.isEmpty()) {
+            sb.append(q.pop());
+        }
+
+        System.out.println(sb);
     }
 }
