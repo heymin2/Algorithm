026a57fd5e9156e1a2dab8185ed00960cc7d26c0
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-16 17:49:52 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -5,31 +5,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int X = Integer.parseInt(br.readLine());
-       int result = 0;
+        String s = Integer.toBinaryString(X);
 
-        while(true) {
-            if(X == 64 || X == 32 || X == 16 || X == 8 || X == 4 || X == 2 || X == 1) {
-                System.out.println(result+1);
-                break;
-            }
-            else {
-                if(32 < X && X < 64) {
-                    X -= 32;
-                }
-                else if(16 < X && X < 32) {
-                    X -= 16;
-                }
-                else if(8 < X && X < 16) {
-                    X -= 8;
-                }
-                else if(4 < X && X < 8) {
-                    X -= 4;
-                }
-                else if(2 < X && X < 4) {
-                    X -= 2;
-                }
-                result++;
-            }
+        int result = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1') result++;
         }
+
+        System.out.println(result);
     }
 }
