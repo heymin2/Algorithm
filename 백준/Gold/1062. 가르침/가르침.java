a904a9ccc45e724d294384d980ea72dd07379b10
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-18 11:22:08 +0900

    [Gold IV] Title: 가르침, Time: 396 ms, Memory: 16676 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, K;
+    static List<String> words;
+    static boolean[] alpha = new boolean[26];
+    static int max = 0, check = 0;
+
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 단어의 개수
+        K = Integer.parseInt(st.nextToken()); // 가르칠 수 있는 글자수
+
+        words = new ArrayList<>(); // 남극 단어
+        
+        for(int i = 0; i < N; i++) {
+            String input = br.readLine();
+
+            if(input.equals("antatica")) check++;
+            else {
+                String[] splitInput = input.split("anta");
+                String[] splitInput2 = splitInput[1].split("tica");
+                words.add(splitInput2[0]);
+            }
+        }
+
+        // 풀이
+        if(K < 5) System.out.println(0); // 기본으로 익혀야하는 글자 a n t i c (5개)
+        else {
+            alpha['a' - 'a'] = true;
+            alpha['n' - 'a'] = true;
+            alpha['t' - 'a'] = true;
+            alpha['i' - 'a'] = true;
+            alpha['c' - 'a'] = true;
+
+            dfs(0, 0);
+            System.out.println(max+check);
+        }
+        
+    }
+
+    static void dfs(int idx, int cnt) {
+        if(cnt + 5 == K) {
+            countWord();
+            return;
+        }
+
+        for(int i = idx; i < 26; i++) {
+            if(alpha[i]) continue;
+
+            alpha[i] = true;
+            dfs(i+1, cnt+1);
+            alpha[i] = false;
+        }
+    }
+
+    static void countWord() {
+        int cnt = 0;
+        for(int i = 0; i < N - check; i++) {
+            boolean flag = true;
+            for(int j = 0; j < words.get(i).length(); j++) {
+                if(!alpha[words.get(i).charAt(j) - 'a']) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                cnt++;
+            }
+        }
+
+        max = Math.max(max, cnt);
+    }
+}
