85fe51f527a69840c945bfba971e7c76a3ac2a76
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-24 16:26:04 +0900

    [Gold V] Title: 괄호의 값, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,56 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        Stack<Character> s = new Stack<>();
-
+        String input = br.readLine();
+        ArrayDeque<Character> stack = new ArrayDeque<Character>();
         int result = 0;
-        int sum = 1;
 
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '(') {
-                s.push('(');
+        int cnt = 1;
+        for(int i = 0; i < input.length(); i++) {
+            char now = input.charAt(i);
 
-                sum *= 2;
+            if(now == '(') {
+                stack.push('(');
+                cnt *= 2;
             }
-            else if(str.charAt(i) == '[') {
-                s.push('[');
-
-                sum *= 3;
+            else if(now == '[') {
+                stack.push('[');
+                cnt *= 3;
             }
-            else if(str.charAt(i) == ')') {
-                if(s.isEmpty() || !s.contains('(')) {
+            else if(now == ')') {
+                if(stack.isEmpty() || !stack.contains('(')) {
                     result = 0;
                     break;
                 }
-                else if(str.charAt(i-1) == '(') {
-                    result += sum;
+                else if(input.charAt(i-1) == '(') {
+                    result += cnt;
                 }
-                sum /= 2;
-                s.pop();
-			}
-			else if(str.charAt(i) == ']') {
-				if(s.isEmpty() || !s.contains('[')) {
-					result = 0;
-					break;
-				}
-				else if(str.charAt(i-1) == '['){
-					result += sum;
-				}
-				sum /= 3;
-				s.pop();
-			}
+                cnt /= 2;
+                stack.pop();
+            }
+            else if(now == ']') {
+                if(stack.isEmpty() || !stack.contains('[')) {
+                    result = 0;
+                    break;
+                }
+                else if(input.charAt(i-1) == '[') {
+                    result += cnt;
+                }
+                cnt /= 3;
+                stack.pop();
+            }
         }
 
-        if(!s.isEmpty()) {
+        if(!stack.isEmpty()) {
             result = 0;
         }
-        
+
         System.out.println(result);
     }
 }
