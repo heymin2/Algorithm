8cc98839b17021d922b5cc9aac26b5d7172b7e23
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-27 13:41:09 +0900

    [Gold V] Title: 선 긋기, Time: 1364 ms, Memory: 282640 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        List<Node> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new Node(x, y));
+        }
+
+        list.sort(null);
+
+        int first = list.get(0).x;
+        int last = list.get(0).y;
+        int result = 0;
+
+        for(int i = 1; i < N; i++) {
+            Node now = list.get(i);
+
+            if(last >= now.x && last < now.y) {
+                last = now.y;
+            }
+            else if(last < now.x) {
+                result += last - first;
+                first = now.x;
+                last = now.y;
+            }
+        }
+
+        result += last - first;
+        System.out.println(result);
+    }
+}
+
+class Node implements Comparable<Node> {
+    int x, y;
+
+    public Node(int x, int y) { 
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if (this.x == o.x) {
+            return o.y - this.y;
+        }
+        return this.x - o.x;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+}
