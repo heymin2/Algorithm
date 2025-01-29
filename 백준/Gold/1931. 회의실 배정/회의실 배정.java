138fc81cba16f97e746ce2140559dac6fb4168a1
혜민 <97522726+heymin2@users.noreply.github.com>
2025-01-29 12:07:10 +0900

    [Gold V] Title: 회의실 배정, Time: 472 ms, Memory: 44564 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -6,45 +6,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Office> caws = new ArrayList<>();
+        int[][] arr = new int[N][2];
 
         StringTokenizer st;
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            caws.add(new Office(start, end));
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        caws.sort(null);
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
 
-        int result = 0; int cnt = 0;
-        for(int i = 0; i < N; i++) {
-            Office now = caws.get(i);
-
-            if(result <= now.start) {
-                result = now.end;
+        int result = arr[0][1];
+        int cnt = 1;
+        for (int i = 1; i < N; i++) {
+            if (result <= arr[i][0]) {
+                result = arr[i][1];
                 cnt++;
             }
         }
 
         System.out.println(cnt);
-    }
-}
-
-class Office implements Comparable<Office>{ 
-    int start, end;
-
-    public Office(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public int compareTo(Office o) {
-        if(this.end == o.end) {
-            return this.start - o.start;
-        }
-        return this.end - o.end;
     }
 }
