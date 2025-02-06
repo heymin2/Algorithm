0e49b3888894be3bc46b4d100cf09115fca20dc6
혜민 <97522726+heymin2@users.noreply.github.com>
2025-02-06 13:45:53 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,54 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M, arr[], check[];
-	static boolean[] visit;
-	static StringBuilder sb;
+    static int N, M;
+    static int[] arr, value;
+    static boolean[] check;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	sb = new StringBuilder();
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[N];
-    	check = new int[M];
-    	visit = new boolean[N];
-    	
-    	st = new StringTokenizer(br.readLine());
-    	for(int i = 0; i < N; i++) {
-    		arr[i] = Integer.parseInt(st.nextToken());
-    	}
-    	
-    	Arrays.sort(arr);
-    	
-    	re(0,0);
-    	System.out.println(sb);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        value = new int[N];
+        for(int i = 0; i < N; i++) {
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        arr = new int[M];
+        check = new boolean[N+1];
+
+        sb = new StringBuilder();
+
+        Arrays.sort(value);
+        re(0, 0);
+        System.out.println(sb);
     }
-    
-    static void re(int cnt, int idx) {
-    	if(cnt == M) {
-    		for(int i = 0; i < M; i++) {
-    			sb.append(check[i]).append(" ");
-    		}
-    		
-    		sb.append("\n");
-    		return;
-    	}
-    	
-    	for(int i = idx; i < N; i++) {
-    		if(!visit[i]) {
-    			visit[i] = true;
-    			check[cnt] = arr[i];
-        		re(cnt+1, i);
-        		visit[i] = false;
-    		}
-    	}
+
+    static void re(int idx, int cnt) {
+        if(idx == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i = cnt; i < N; i++) {
+            arr[idx] = value[i];
+            re(idx+1, i+1);
+        }
     }
 }
