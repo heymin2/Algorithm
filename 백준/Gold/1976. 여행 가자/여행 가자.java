0ef0b66e2bdab93e643a055ad117c41f4240b99e
혜민 <97522726+heymin2@users.noreply.github.com>
2025-02-06 14:07:27 +0900

    [Gold IV] Title: 여행 가자, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,69 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, arr[];
+    static int N, M;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine()); // 도시 수
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 도시의 수
+        int M = Integer.parseInt(br.readLine()); // 여행 계획에 속한 도시의 수
 
-        arr = new int[N + 1];
+        parent = new int[N+1];
 
-        for(int i = 1; i <= N; i++){
-            arr[i] = i;
+        for(int i = 1; i <= N; i++) {
+            parent[i] = i;
         }
 
-        for(int i = 1; i <= N; i++){
+        for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for(int j = 1; j <= N; j++){
-                int x = Integer.parseInt(st.nextToken());
-
-                if(x == 1){
+            for(int j = 1; j <= N; j++) {
+                int check = Integer.parseInt(st.nextToken());
+                if(check == 1) {
                     union(i, j);
                 }
-            }   
+            }
         }
-
+        
         st = new StringTokenizer(br.readLine());
-        String result = "YES";
-
-        int prev = find(Integer.parseInt(st.nextToken()));
-
-        for (int j = 1; j < M; j++) {
-            int now = Integer.parseInt(st.nextToken());
-
-            if(prev != find(now)){
-                result = "NO";
+        int n = find(Integer.parseInt(st.nextToken()));
+        boolean flag = true;
+        for(int i = 1; i < M; i++) {
+            if(n != find(Integer.parseInt(st.nextToken()))) {
+                flag = false;
+                System.out.println("NO");
                 break;
             }
         }
 
-        System.out.println(result);
+        if(flag) System.out.println("YES");
     }
-    
+
     static void union(int x, int y) {
         x = find(x);
         y = find(y);
 
-        if (x == y)
-            return;
-
-        if(x < y) arr[y] = x;
-        else arr[x] = y;
+        if(x > y) parent[y] = x;
+        else parent[x] = y;
     }
-    
+
     static int find(int x) {
-        if (x == arr[x])
+        if(x == parent[x]) {
             return x;
-        
-        return find(arr[x]);
+        }
+        else return parent[x] = find(parent[x]);
     }
 }
