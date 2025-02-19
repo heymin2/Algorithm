1cf07837db63d96377f38388f2099052043eb3e3
혜민 <97522726+heymin2@users.noreply.github.com>
2025-02-19 14:08:22 +0900

    [Silver III] Title: 블로그, Time: 320 ms, Memory: 35652 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -25,25 +25,22 @@ public class Main {
 
         int left = 0;
         int right = X;
+        int result = sum[0];
+        int cnt = 1;
 
         for(int i = 1; i < N-X+1; i++) {
-            total = total + arr[right] - arr[left];
+            total += arr[right++] - arr[left++];
             sum[i] = total;
-            right++; left++;
+
+            if(sum[i] > result) {
+                result = sum[i];
+                cnt = 1;
+            }
+            else if(sum[i] == result) cnt++;
         }
-
-        Arrays.sort(sum);
-
-        int result = sum[N-X];
 
         if(result == 0) System.out.println("SAD");
         else {
-            int cnt = 0;
-            for(int i = N-X; i >= 0; i--) {
-                if(sum[i] == result) cnt++;
-                else break; 
-            }
-    
             StringBuilder sb = new StringBuilder();
             sb.append(result).append("\n").append(cnt);
     
