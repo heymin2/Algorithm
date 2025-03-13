bdd106be5b2693000e23ce944fe2855ab1909377
혜민 <97522726+heymin2@users.noreply.github.com>
2025-03-13 22:06:58 +0900

    [Silver V] Title: 날짜 계산, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int e = 0, s = 0, m = 0, year = 0;
+        while(true) {
+            e++; s++; m++; year++;
+
+            if(e > 15) e = 1;
+            if(s > 28) s = 1;
+            if(m > 19) m = 1;
+
+            if(e == E && s == S && m == M) {
+                break;
+            }
+        }
+
+        System.out.println(year);
+    }
+}
